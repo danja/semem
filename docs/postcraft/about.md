d51031bc5cbd691082424c65fdb8643c44d6712a
Danny Ayers <danny.ayers@gmail.com>
2025-05-17 11:13:44 +0200

    doc support

---
+++ docs/postcraft/about.md
@@ -0,0 +1,26 @@
+# About
+
+## Default Postcraft Structure
+
+- about.md : root description
+- app.ttl : Transmissions settings
+- content 
+  - media
+  - raw
+  - static
+- knowledge
+  - prompts
+  - references
+- layout
+  - base
+  - logos
+- public
+- system
+  - sparql-templates
+
+---
+
+```sh
+cd ~/sites/strandz.it/postcraft
+tree -L 2 --filesfirst
+```
--- embedding-creator.js
+++ embedding-creator.js
@@ -10,8 +10,8 @@ logger.setLevel('info');
 // Parse command line arguments
 const args = process.argv.slice(2);
 let limit = 0;
-let graphName = 'http://danny.ayers.name/content';
-
+let graphName = 'http://hyperdata.it/content';
+// let graphName = 'http://danny.ayers.name/content';
 // Simple argument parsing
 for (let i = 0; i < args.length; i++) {
     if (args[i] === '--limit' && i + 1 < args.length) {
