40b28f1827abb54268357c311e11fa6981f2ceb9
Danny Ayers <danny.ayers@gmail.com>
2025-09-19 11:06:57 +0200

    tidy

--- examples/pending/loadRagnoConfig.js
+++
@@ -1,9 +0,0 @@
-// Utility: Load and parse ragno-config.json
-import fs from 'fs/promises';
-import path from 'path';
-
-export default async function loadRagnoConfig() {
-  const configPath = path.resolve('docs/ragno/ragno-config.json');
-  const data = await fs.readFile(configPath, 'utf-8');
-  return JSON.parse(data).ragno;
-}
